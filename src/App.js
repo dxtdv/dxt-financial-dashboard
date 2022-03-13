ca74c80d470424b7efc6df860a69a79e2ec7419c
lakhal84 <probusilakhal@gmail.com>
2022-03-13 02:00:30 +0100

    Create comming soon page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div style={{
+      display:'flex',
+      flexDirection:'column',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'100vh',
+      fontFamily:'roboto'
 
-export default App;
+    }}>
+      <h1 style={{
+        color:'#6EC177',
+        margin: 0
+      }}>This is a financial dashbord</h1>
+      <h2>Comming soon!</h2>
+    </div>
+  )
+}
